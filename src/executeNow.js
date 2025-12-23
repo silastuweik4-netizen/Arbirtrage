bca060b4618f8736a2d25b5b7f8bac32efc57c93
silastuweik4-netizen <silastuweik4@gmail.com>
2025-12-23 21:58:27 +0300

    Delete src directory

--- src/executeNow.js
+++
@@ -1,6 +0,0 @@
-import { executeOne } from './oneShot.js';
-import { config } from 'dotenv'; config();
-
-// execute the exact mint from the trending ping
-const MINT = '6vVfbQVRSXcfyQamPqCzcqmA86vCzb2d7B7gmDDqpump'; // copy from Telegram
-executeOne(MINT, 1_000_000).catch(e => console.log('One-shot crash', e));
