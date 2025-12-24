8245e7d0cfae6bed15a0c3ff6568c4de256ce00d
silastuweik4-netizen <silastuweik4@gmail.com>
2025-12-24 14:23:48 +0300

    Delete src directory

--- src/wallet.js
+++
@@ -1,8 +0,0 @@
-import { Keypair } from '@solana/web3.js';
-import bs58 from 'bs58';
-
-export function getKeypair() {
-  const key = process.env.PRIVATE_KEY_BASE58;
-  if (!key) throw new Error('PRIVATE_KEY_BASE58 missing');
-  return Keypair.fromSecretKey(bs58.decode(key.trim()));
-}
