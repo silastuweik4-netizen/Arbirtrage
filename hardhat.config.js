4adf3ab9eccddd4f48d31d4dbc12fb686e0ed5b6
silastuweik4-netizen <silastuweik4@gmail.com>
2026-01-21 18:50:06 +0300

    Create hardhat.config.js

---
+++ hardhat.config.js
@@ -0,0 +1,11 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+module.exports = {
+  solidity: "0.8.20",
+  networks: {
+    base: {
+      url: process.env.RPC_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  }
+};
