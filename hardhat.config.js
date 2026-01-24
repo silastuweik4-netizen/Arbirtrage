5fc397792bcb19bb4f8e7b2a524e82c6214b8ac1
silastuweik4-netizen <silastuweik4@gmail.com>
2026-01-24 07:50:09 +0300

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,21 @@
 require("@nomicfoundation/hardhat-toolbox");
 
 module.exports = {
-  solidity: "0.8.20",
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: { enabled: true, runs: 200 }
+    }
+  },
   networks: {
     base: {
       url: process.env.RPC_URL,
       accounts: [process.env.PRIVATE_KEY]
+    }
+  },
+  etherscan: {
+    apiKey: {
+      base: process.env.BASESCAN_API_KEY
     }
   }
 };
